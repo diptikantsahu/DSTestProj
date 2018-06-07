8d4a549563a262ea9babf5e2fe6c11af4e63e826
diptikantsahu <diptikant.sahu@live.com>
2018-06-07 15:11:32 -0700

    ArrayRotation
    Rotate an array by one or N number

---
+++ Array/Array/ArrayRotation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class ArrayRotation
+    {
+        public static void RotateArrayByOne(int [] arr)
+        {
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i+1] = temp;
+            }
+        }
+
+        public static int[] RotateArrayByN(int [] arr, int number)
+        {
+            for (int i = 0;i < number;i++)
+            {
+                RotateArrayByOne(arr);
+            }
+
+            return arr;
+        }
+    }
+}
--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -11,9 +11,29 @@ namespace Array
         static void Main(string[] args)
         {
 
+            //Array sample for rotation
+            int[] arr = { 2, 3, 6, 5, 9 };
+
             //Array rotation
+            int[] arr1;
+
+           arr1 = ArrayRotation.RotateArrayByN(arr,2);
+
+            PrintArray(arr1);
+
+            Console.Read();
 
         }
+
+        public static void PrintArray(int [] arr)
+        {
+            for(int i = 0; i< arr.Length;i++ )
+            {
+                Console.WriteLine("{0},",arr[i]);
+            }
+        }
+
+        
     }
 
     
